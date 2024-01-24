2c5b9296fbf2d07cc84e2c6475b51fde5a7bd7f5
Paulo Henrique <paulo@MacBook-Pro-de-Ivo.local>
2024-01-24 16:52:47 -0300

    feat: adding validation configuration to the project with some pipe verbs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //passando a configuracao da validacao e as configuracoes personalizadas
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, //fica responsável por transformar o json no DTO criado para poder ser validade
+      whitelist: true, //ignora de forma silenciosa qualquer propriedade que não está no DTO e não valida as mesmas.
+      forbidNonWhitelisted: true, //basicamente faz com que não seja aceito nenhuma propriedade previamente não declarada
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
